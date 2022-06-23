566faf6204871ce0cdaf1699d65a440a5e665eae
l-Bohdan-l <bpapushoj@gmail.com>
2022-06-23 22:44:04 +0300

    total

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <div>
+          <input type="text" placeholder="Search for weather" />
+          <button type='button' >Search</button>
+        </div>
+        <div>
+          <h1>Weather</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
