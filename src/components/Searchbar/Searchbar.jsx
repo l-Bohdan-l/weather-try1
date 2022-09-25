cb9de0b0a7c56b9eda4339df4a4879b9e24f75da
l-Bohdan-l <bpapushoj@gmail.com>
2022-09-25 18:44:24 +0300

    1

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,25 +1,20 @@
 /* eslint-disable no-unused-vars */
-import {Input, ClearButton} from './Searchbar.styles';
+import {Header, Form, Button, Input} from './Searchbar.styled';
+import {FcSearch} from 'react-icons/fc';
 
 export function Searchbar() {
     return (
-        // <div className="searchbar">
-        //     <input type="text" placeholder="Search for weather" />
-        //     <button type='button' >Search</button>
-        // </div>
+        <Header>
+            <Form>
+                <Input         
+                type="text"
+                placeholder="Search weather by city"
+                />
 
-        <Input>
-            <div id="text">
-                <input type="text" placeholder="Search for weather" />
-            </div>
-            <ClearButton id="clear">
-                <svg viewBox="0 0 24 24">
-                    <path id="line" d="M2 2L22 22" />
-                    <path id="long" d="M9 15L20 4" />
-                    <path id="arrow" d="M13 11V7" />
-                    <path id="arrow" d="M17 11H13" />
-                </svg>
-            </ClearButton>
-        </Input>
+                <Button type="submit">
+                <FcSearch/>
+                </Button>
+            </Form>
+        </Header>
     )
 }
